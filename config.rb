1e4d87dcc1fbfad3f9c78f19c2de3a1f462376a1
Daniel Gamage <hello@danielgamage.com>
2014-01-30 22:44:10 -0500

    config.rb

---
+++ config.rb
@@ -0,0 +1,26 @@
+# Location of the theme's resources.
+css_dir         = "css"
+sass_dir        = "sass"
+images_dir      = "img"
+javascripts_dir = "js"
+
+
+# Require any additional compass plugins installed on your system.
+require 'modular-scale'
+require 'breakpoint'
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = (environment == :development) ? :expanded : :compressed
+
+# To enable relative paths to assets via compass helper functions. Since Drupal
+# themes can be installed in multiple locations, we don't need to worry about
+# the absolute path to the theme from the server root.
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+# Pass options to sass. For development, we turn on the FireSass-compatible
+# debug_info if the firesass config variable above is true.
+sass_options = (environment == :development && firesass == true) ? {:debug_info => true} : {}
