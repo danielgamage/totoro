7d6242bb9d62afb4169ad0834fe7d375bf8d1a94
Daniel Gamage <hello@danielgamage.com>
2014-01-30 22:46:05 -0500

    cute update nighttime only

--- index.php
+++ index.php
@@ -22,14 +22,16 @@
         <![endif]-->
         <header>
             <h1 lang="ja">となりの<span>トトロ<span></h1>
-             <?php include "img/clouds.svg" ?>
+             <?php // include "img/clouds.svg" ?>
             <?php include "img/totorosm.svg" ?>
             <?php include "img/sign.svg" ?>
         </header>
-        <section>
-            <p>Growing up, kids always have movies that stick with them until they grey. Me, I wasn't too interested in movies. I saw the blockbusters when they came to the local theatre, but it's hard to the excitement or the imersive feeling of being in another world from films like <em>Scary Movie II</em>. It just doesn't happen, and it wasn't until I turned 16 that I saw a film called <em>My Neighbor Totoro</em>, and it was one of the first movies I saw and felt really good in just watching it.
-            <p>I always remember the film in retrospect as being vibrant and brimming with lively music; nothing but visual chaos that easily translates into a certain overwhelming ecstasy for the viewer. But, uh, I watched it again recently, and it's pretty dull in terms of saturation. Scenes are generally long, giving it a drawn-out feel at first, but really it's just a more realistic pace. Life doesn't happen as quickly as it seems to in a Hollywood film. So neither of those memories of the movie are technically true, but I think what the movie lacks in terms of overwhelming visual stimuli, it maintains a similar feeling as a result of its style. 
-        </section>
+        <article>
+            <div class="wrapper">
+                <p>Growing up, kids always have movies that stick with them until they grey. Me, I wasn't too interested in movies. I saw the blockbusters when they came to the local theatre, but it's hard to the excitement or the imersive feeling of being in another world from films like <em>Scary Movie II</em>. It just doesn't happen, and it wasn't until I turned 16 that I saw a film called <em>My Neighbor Totoro</em>, and it was one of the first movies I saw and felt really good in just watching it.
+                <p>I always remember the film in retrospect as being vibrant and brimming with lively music; nothing but visual chaos that easily translates into a certain overwhelming ecstasy for the viewer. But, uh, I watched it again recently, and it's pretty dull in terms of saturation. Scenes are generally long, giving it a drawn-out feel at first, but really it's just a more realistic pace. Life doesn't happen as quickly as it seems to in a Hollywood film. So neither of those memories of the movie are technically true, but I think what the movie lacks in terms of overwhelming visual stimuli, it maintains a similar feeling as a result of its style.
+            </div>
+        </article>
         <div class="container">
             <?php include "img/dust.svg" ?>
             <?php include "img/dust.svg" ?>
@@ -49,7 +51,7 @@
                 <h3 lang="ja">2013 ダニエルガマジュ</h3>
                 <p lang="ja">お世話になりました、トトロ。</p>
         </footer>
-        <script src="js/main.js"></script>
+        <?php // <script src="js/main.js"></script> ?>
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
         <script>
             var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
